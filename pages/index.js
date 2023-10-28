c904ba86c20a48886dc0ff49817b828f5c881981
jiyoon <vywns9978@gmail.com>
2023-10-29 00:28:54 +0900

    📌 Link 컴포넌트
    href 속성을 문자열로 전달하는 방법과 url object를 사용하는 방법

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  const code = "KOR";
+
+  return (
+    <div>
+      Home Page
+      <div>
+        <Link href={"/search"}>Search Page 이동</Link>
+      </div>
+      <div>
+        {/* <Link href={`/country/${code}`}>{code} Page 이동</Link> */}
+        <Link
+          href={{
+            pathname: "/country/[code]",
+            query: { code: code },
+          }}
+        >
+          {code} Page 이동
+        </Link>
+      </div>
+    </div>
+  );
 }
