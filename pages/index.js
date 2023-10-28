44bb1d13804a749e2b94f6c7efe5513b0f50eebc
jiyoon <vywns9978@gmail.com>
2023-10-29 00:53:16 +0900

    📌 useRouter의 push 메서드 사용 및 문자열과 url object 인수로 사용

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const code = "KOR";
+  const router = useRouter();
+
+  const onClickButton = () => {
+    // router.push("/search");
+    router.push({
+      pathname: "/country/[code]",
+      query: { code: code },
+    });
+  };
 
   return (
     <div>
       Home Page
       <div>
+        <button onClick={onClickButton}>Country 페이지로 이동</button>
         <Link href={"/search"}>Search Page 이동</Link>
       </div>
       <div>
