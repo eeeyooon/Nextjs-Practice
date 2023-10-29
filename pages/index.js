4408f74358821cfe30d3b589ae667d7c5f565009
jiyoon <vywns9978@gmail.com>
2023-10-29 23:46:28 +0900

    📌 SSR 적용해보기
    - `getServerSideProps` 함수 사용
    - 서버측과 클라이언트 측에서 실행되는 코드 확인

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,23 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function Home() {
-  const code = "KOR";
-  const router = useRouter();
+export default function Home({ name }) {
+  console.log("HOME");
 
-  const onClickButton = () => {
-    // router.push("/search");
-    router.push({
-      pathname: "/country/[code]",
-      query: { code: code },
-    });
-  };
+  useEffect(() => {
+    console.log("HOME MOUNT");
+  }, []);
 
-  return (
-    <div>
-      Home Page
-      <div>
-        <button onClick={onClickButton}>Country 페이지로 이동</button>
-        <Link href={"/search"}>Search Page 이동</Link>
-      </div>
-      <div>
-        {/* <Link href={`/country/${code}`}>{code} Page 이동</Link> */}
-        <Link
-          href={{
-            pathname: "/country/[code]",
-            query: { code: code },
-          }}
-        >
-          {code} Page 이동
-        </Link>
-      </div>
-    </div>
-  );
+  return <div>{name}</div>;
 }
+
+export const getServerSideProps = async () => {
+  // SSR을 위해 서버측에서 컴포넌트(페이지의 역할을 하는 컴포넌트 <- 여기선 Home 컴포넌트)에게 전달할 데이터를 설정하는 함수.
+  // 이 함수는 반드시 객체를 반환해야 함. props와 props의 값(무조건 객체)
+  // 이 객체의 값은 반드시 컴포넌트로 전달됨.
+
+  return {
+    props: {
+      name: "KOREA",
+    },
+  };
+};
