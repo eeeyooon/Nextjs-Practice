e2036c4593c30c3bb36199f1a9accc1162f1ee5f
jiyoon <vywns9978@gmail.com>
2023-10-30 23:48:46 +0900

    📌모든 페이지에 SSR 전략 적용 및 서버에서 API 호출하여 렌더링하기

--- pages/country/[code].js
+++ pages/country/[code].js
@@ -1,17 +1,33 @@
+import { fetchCountry } from "@/api";
 import SubLayout from "@/components/SubLayout";
 import { useRouter } from "next/router";
 // Next에서 제공하는 커스텀 훅. router 객체를 반환하는 함수인데,
 // router 객체 안에 라우팅과 관련된 대부분의 정보가 다 저장되어있음.
 
-export default function Country() {
+export default function Country({ country }) {
   const router = useRouter();
-  // router 변수에 useRouter 객체 저장
-
-  // const code = router.query.code;
   const { code } = router.query;
   // code 변수에 url 파라미터로 전달된 국가코드 저장.
 
-  return <div>Country code : {code}</div>;
+  return (
+    <div>
+      {country.commonName} {country.officialName}
+    </div>
+  );
 }
 
 Country.Layout = SubLayout;
+
+export const getServerSideProps = async (context) => {
+  const { code } = context.params;
+
+  let country = null;
+
+  if (code) {
+    country = await fetchCountry(code);
+  }
+
+  return {
+    props: { country },
+  };
+};
