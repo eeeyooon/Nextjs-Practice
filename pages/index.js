a55e1e6956301c7954a1340f6c18b81e948411e2
jiyoon <vywns9978@gmail.com>
2023-10-30 23:26:23 +0900

    📌 API 호출하여 국가 데이터 렌더링 및 SSR 방식에서의 api 호출 방식 확인

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import { useEffect } from "react";
+import { fetchCountries } from "@/api";
 
-export default function Home({ name }) {
-  console.log("HOME");
-
-  useEffect(() => {
-    console.log("HOME MOUNT");
-  }, []);
-
-  return <div>{name}</div>;
+export default function Home({ countries }) {
+  return (
+    <div>
+      {countries.map((country) => (
+        <div key={country.code}>{country.commonName}</div>
+      ))}
+    </div>
+  );
 }
 
 export const getServerSideProps = async () => {
-  // SSR을 위해 서버측에서 컴포넌트(페이지의 역할을 하는 컴포넌트 <- 여기선 Home 컴포넌트)에게 전달할 데이터를 설정하는 함수.
-  // 이 함수는 반드시 객체를 반환해야 함. props와 props의 값(무조건 객체)
-  // 이 객체의 값은 반드시 컴포넌트로 전달됨.
+  // NARAS API 호출 코드
+
+  // 전체 국가 데이터 불러오기
+  const countries = await fetchCountries();
 
   return {
     props: {
-      name: "KOREA",
+      countries,
     },
   };
 };
