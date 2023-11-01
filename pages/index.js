ec672a289d48992f6660033a68595c08c1afb85f
jiyoon <vywns9978@gmail.com>
2023-11-01 20:24:01 +0900

    📌 SSG 적용하기
    getStaticProps 함수 사용하기, SSR 방식과의 차이점 확인하기

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,12 @@ export default function Home({ countries }) {
   );
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // NARAS API 호출 코드
 
   // 전체 국가 데이터 불러오기
   const countries = await fetchCountries();
+  console.log("countries 데이터 불러옴.");
 
   return {
     props: {
