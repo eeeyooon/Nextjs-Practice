d14eb3612fb4b584cad61d55488c99b16a2102a5
jiyoon <vywns9978@gmail.com>
2023-11-04 23:51:25 +0900

    📌 index페이지에 open graph 설정 및 파비콘 적용하기

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Link from "next/link";
 
 export default function Document() {
   return (
     <Html lang="kr">
-      <Head />
+      <Head>
+        <Link type="image" rel="icon" href="/favicon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
